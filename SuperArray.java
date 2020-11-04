ccb7eb244630fb5bc4a19740ac6e5f15f639a561
bittergourdking <jkang21@stuy.edu>
2020-11-04 11:49:40 -0500

    added isEmpty, clear, toString, contains, and new constructor

--- SuperArray.java
+++ SuperArray.java
@@ -7,6 +7,10 @@ public class SuperArray {
     data = new String[10];
     size = 0;
   }
+  public SuperArray(int initialCapacity) {
+    data = new String[initialCapacity];
+    size = 0;
+  }
   //methods
   public int size() {
     return size;
@@ -32,7 +36,40 @@ public class SuperArray {
     data[index] = element;
     return replaced;
   }
-  
+  //new methods!
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+
+  public void clear () {
+    int cap = data.length;
+    data = new String[cap];
+    size = 0;
+  }
+
+  public String toString() {
+    if (size == 0) {
+      return "[]";
+    }
+    String answer = "[";
+    for (int i = 0; i < size - 1; i++) {
+      answer += data[i] + ", ";
+    }
+    answer += data[size - 1] + "]";
+    return answer;
+  }
+
+  public boolean contains(String s) {
+    int len = s.length();
+    String whole = data.toString();
+    for (int i = 0; i < len; i++) {
+      if (whole.substring(i, i + len).equals(s)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   private void resize() {
     String[] larger = new String[data.length + 2];
     for (int i = 0; i < size; i++) {
